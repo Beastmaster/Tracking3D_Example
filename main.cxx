ed3e9686473114ae55774577461ff1d0d895dc52
QIN Shuo <qinshuo0370@gmail.com>
2016-01-08 18:27:17 +0800

    new

--- main.cxx
+++ main.cxx
@@ -147,25 +147,23 @@ void TestRegistration()
 void TestTrackingMarkFunction()
 {
 	
-	auto track = vtkSmartPointer<vtkTracking3D>::New();
+	auto track = new PloarisVicraConfiguration ;
 	
-	auto mark = vtkSmartPointer< vtkTrackingMarkCapture<vtkTracking3D> >::New();
+	auto mark = vtkSmartPointer< vtkTrackingMarkCapture<PloarisVicraConfiguration> >::New();
 	
 	mark->SetTracker(track);
 	mark->SetToolIndex(1);
 	mark->SetReferIndex(2);
 
-	track->InstallPipeline();
-	track->m_tracker->ConfigureTracker();
-	Sleep(20);
-	track->m_tracker->StartTracking();
-	Sleep(20);
+	track->ConfigureTracker();
+	track->StartTracking();
 	
 	while (getchar()!='q')
 	{
 		mark->GetNextMarker();
 	}
 
+	track->StopTracking();
 }
 
 

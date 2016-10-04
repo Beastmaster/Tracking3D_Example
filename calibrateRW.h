3264ef64c08a8035be5f8815d956d82588d3b5b2
QIN Shuo <qinshuo0370@gmail.com>
2016-10-04 14:39:55 +0800

    main function of reading and writing calibration info ( not implemented)

---
+++ calibrateRW.h
@@ -0,0 +1,40 @@
+/*
+Author: QIN Shuo
+Organization: RC-MIC (CUHK)
+Date: 2016/9/29
+
+Description:
+	read and write calibration matrix
+
+*/
+
+#include <string.h>
+#include <string>
+
+class calibrateRW
+{
+	calibrateRW(){};
+	~calibrateRW(){};
+
+
+
+
+private:
+	std::string m_FileName();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
